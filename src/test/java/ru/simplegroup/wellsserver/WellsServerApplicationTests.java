e338af2a60f80569e31ebab7ff76a18deb837aeb
Slipeer <slipeer1@list.ru>
2022-09-08 13:28:09 +0500

    Initial commit

---
+++ src/test/java/ru/simplegroup/wellsserver/WellsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.simplegroup.wellsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
